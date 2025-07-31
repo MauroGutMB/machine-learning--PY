62375648fb01f65c477c96151f5cbdfe9b2fa172
MauroGutMB <mgmbarros2006@gmail.com>
2025-07-31 20:54:14 -0300

    add venv ++

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ correlacao = data.corr()
 
 # print("\n\n\n", correlacao['preco_de_venda'])
 
-plt.scatter(data['area_primeiro_andar'], data['preco_de_venda'])
-plt.title("Relação entre Área e Preço")
-plt.axline(xy1 = (66, 250000), xy2 = (190, 1800000), color = "red")
-plt.xlabel("Área do primeiro andar")
-plt.ylabel("Preço de venda")
 
-plt.savefig("areaXpreco.png", dpi=300, bbox_inches="tight")
+# plt.scatter(data['area_primeiro_andar'], data['preco_de_venda'])
+# plt.title("Relação entre Área e Preço")
+# plt.axline(xy1 = (66, 250000), xy2 = (190, 1800000), color = "red")
+# plt.xlabel("Área do primeiro andar")
+# plt.ylabel("Preço de venda")
+
+img = px.scatter(data, x = 'area_primeiro_andar', y = 'preco_de_venda', trendline_color_override="red", trendline = 'ols' )
+
+# plt.savefig("areaXpreco-px.png")
+# img.write_image("areaXpreco-px.png")
+img.show()
